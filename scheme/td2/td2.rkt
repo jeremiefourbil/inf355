3a37903503e21966a8388bac9c7495d43188deb4
Jérémie FOURBIL <jeremie.fourbil@gmail.com>
2013-05-26 17:26:13 +0200

    queens problem

--- scheme/td2/td2.rkt
+++ scheme/td2/td2.rkt
@@ -1,5 +1,15 @@
 #lang racket
 
+(define test
+  (lambda (cond res)
+    (if (equal? cond res)
+        (void)
+        (error "Bad test result"))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Implémentation de l’opérateur angélique ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define fail #f)
 
 (define (reset)
@@ -32,6 +42,10 @@
 
 (reset)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Récupération de l’ensemble des solutions ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define (bag-of f)
   (let ((results '()))
     (if (amb #t #f)
@@ -40,13 +54,38 @@
           (fail))
         (reverse results))))
 
-(bag-of mult)
+;; tests
+
+(test (bag-of mult) '((3 10) (5 6) (6 5) (10 3)))
+
+(reset)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Problème des huits reines ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (check column row placed)
   (for-each (lambda (coord)
               (let ((c (car coord))
                     (r (cdr coord)))
-                (if (eq? c column) (amb) (void))
-                (if (eq? r row) (amb) (void))
-                (if (or (eq? (+ r row) (- r row)) (eq? (+ c column) (- c column))) (amb) (void)))
-              placed)))
+                (if (equal? c column) (amb) (void))
+                (if (equal? r row) (amb) (void))
+                (if (or (equal? (+ r c) (+ row column)) (equal? (- r c) (- row column))) (amb) (void))))
+              placed))
+
+(define (queens)
+  (let loop ((placed '())
+             (column 1))
+    (if (equal? (length placed) 8)
+        placed 
+        (letrec ((row (amb 1 2 3 4 5 6 7 8))
+                 (coord (cons column row)))
+          (check column row placed)
+          (loop (cons coord placed) (+ column 1))))))
+
+;; tests
+(test (length (bag-of queens)) 92)
+
+;;;;;;;;;;;;;;;;
+;; Coroutines ;;
+;;;;;;;;;;;;;;;;
